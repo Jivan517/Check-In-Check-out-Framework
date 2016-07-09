e3fa00a16ecdb72b2dc0bd5366f83d7d7d03aa44
Jivan517 <np.jiv004@gmail.com>
2016-07-09 14:30:04 -0500

    Framework: initial gradle setup added

---
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/middleware/Person.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the MIT License (MIT);
+ * 
+ */
+
+package cs525.project.fujframework.middleware;
+
+/**
+ * generic class for managing the concrete classes for factory method
+ * 
+ * @author Jivan Nepali
+ * 
+ * @since 1.0.0
+ *
+ */
+public interface Person {
+
+}
