490f14eeb61de70ed168c48c7a5903286b39b4ad
Fisseha Abebe <abebefisseha5@gmail.com>
2016-07-10 15:30:46 -0500

    Added check out records protection prexy class(CheckOutRecordProtectionProxy)

--- code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/dataaccess/DbAction.java
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/dataaccess/DbAction.java
@@ -5,36 +5,37 @@
  */
 package cs525.project.fujframework.core.dataaccess;
 /**
- * 
+ * This @{DbAction} class used for performing database actions
+ * by communicating with @{SimpleConnectionPool} 
  * @author Fish
  *
  */
 public interface DbAction {
 	/**
-	 * 
+	 * This method Performs insert query based passed query statement
 	 * @param query
-	 * @return
+	 * @return integer
 	 */
 	public int Create(String query);
 
 	/**
-	 * 
+	 * This method Performs read query based passed query statement
 	 * @param query
-	 * @return
+	 * @return Object 
 	 */
 	public Object read(String query);
 
 	/**
-	 * 
+	 * This method Performs delete query based passed query statement
 	 * @param query
-	 * @return
+	 * @return integer
 	 */
 	public int delete(String query);
 
 	/**
-	 * 
+	 * This method Performs update query based passed query statement
 	 * @param query
-	 * @return
+	 * @return integer
 	 */
 	public int update(String query);
 
