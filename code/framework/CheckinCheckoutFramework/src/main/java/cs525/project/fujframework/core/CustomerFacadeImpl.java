3bbdc39ed8343180c1fec60c3f351245eed1a2a8
Umesh Paudyal <umesh.paudel.nepal@gmail.com>
2016-07-10 14:05:42 -0500

    adds facade implementations

---
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/CustomerFacadeImpl.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package cs525.project.fujframework.core;
+
+import cs525.project.fujframework.middleware.Customer;
+
+/**
+ * @author paudelumesh
+ *
+ */
+public class CustomerFacadeImpl implements CustomerFacade {
+
+	/* (non-Javadoc)
+	 * @see cs525.project.fujframework.core.CustomerFacade#saveCustomer(cs525.project.fujframework.middleware.Customer)
+	 */
+	@Override
+	public boolean saveCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see cs525.project.fujframework.core.CustomerFacade#removeCustomer(cs525.project.fujframework.middleware.Customer)
+	 */
+	@Override
+	public boolean removeCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
