4df7e03dc1cfbf1da8aee735626c54e88a6bdb4d
Umesh Paudyal <umesh.paudel.nepal@gmail.com>
2016-07-09 22:11:27 -0500

    commits initial skeleton for the facade interface

---
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/SysUserFacade.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package cs525.project.fujframework.core;
+
+/**
+ * defines a higher level interface for system user related subsystems
+ * 
+ * @author paudelumesh
+ *
+ * @version 1.0.0
+ */
+public interface SysUserFacade {
+
+	/**
+	 * saves the system user into database taking the request from command
+	 * pattern
+	 * 
+	 * @param sysUser
+	 * @return boolean
+	 */
+	public boolean saveSysUser(SysUser sysUser);
+
+	/**
+	 * removes the system user from database taking the request from command
+	 * pattern
+	 * 
+	 * @param sysUser
+	 * @return boolean
+	 */
+	public boolean removeSysUser(SysUser sysUser);
+}
