b7a474af4d56330a9af4ce7f341401e3140520ff
Fisseha Abebe <abebefisseha5@gmail.com>
2016-07-11 11:58:26 -0500

    Removed unused imports

--- code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/dataaccess/SimpleConnectionPool.java
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/core/dataaccess/SimpleConnectionPool.java
@@ -10,11 +10,6 @@ package cs525.project.fujframework.core.dataaccess;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-
 import cs525.project.fujframework.utils.DbConfig;
 
 /**
@@ -31,6 +26,12 @@ public class SimpleConnectionPool {
 	private SimpleConnectionPool() {
 	}
 
+	/**
+	 * This method returns single instance of @{SimpleConnectionPool} using if
+	 * it is not already created.
+	 * 
+	 * @return instance
+	 */
 	public static SimpleConnectionPool getInstance() {
 		if (instance == null) {
 			instance = new SimpleConnectionPool();
@@ -38,6 +39,13 @@ public class SimpleConnectionPool {
 		return instance;
 	}
 
+	/**
+	 * This method return connection object
+	 * 
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		DbConfig config = new DbConfig();
 		Connection con = null;
