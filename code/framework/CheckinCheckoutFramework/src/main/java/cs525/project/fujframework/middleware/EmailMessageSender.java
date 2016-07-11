5534ec86a797f5bd73e532ede1f47fe282680b10
Fisseha Abebe <abebefisseha5@gmail.com>
2016-07-11 00:18:10 -0500

    Modified Email sender

---
+++ code/framework/CheckinCheckoutFramework/src/main/java/cs525/project/fujframework/middleware/EmailMessageSender.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the MIT License (MIT);
+ */
+package cs525.project.fujframework.middleware;
+import java.util.Properties;
+import cs525.project.fujframework.utils.ConfigProperties;
+import cs525.project.fujframework.utils.ConfigPropertiesImpl;
+
+/**
+ * @author Fish
+ *
+ */
+public class EmailMessageSender implements MessageSender {
+
+	private ConfigProperties config;
+	
+	public EmailMessageSender() {
+		this.config = new ConfigPropertiesImpl("mail.properties");
+	}
+	/* (non-Javadoc)
+	 * @see cs525.project.fujframework.middleware.MessageSender#sendMessage(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void sendMessage(String subject, String body) {
+		final String username = config.readProperty("username");
+		final String password = config.readProperty("password");
+		//final String to="chari.fisseha@gmail.com";
+
+		Properties props = new Properties();
+		props.put(config.readProperty("user"),username); 
+		props.put(config.readProperty("auth"), "true");
+		props.put(config.readProperty("starttls"), "true");		
+		props.put(config.readProperty("portname"), config.readProperty("port"));
+		props.put(config.readProperty("enablessl"),"true");
+		props.put(config.readProperty("hostname"), config.readProperty("host"));
+		
+		props.setProperty(config.readProperty("socketfactoryname"), config.readProperty("socketfactory"));   
+		props.setProperty(config.readProperty("fallback"), "false");   
+		props.setProperty(config.readProperty("smtpportname"), config.readProperty("smtpport"));   
+		props.setProperty(config.readProperty("socketfacprtname"), config.readProperty("socketfacport")); 
+		
+
+		Session session = Session.getInstance(props,new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(to));
+			message.setSubject(subject);
+			message.setText(body);
+
+			Transport.send(message);
+
+			System.out.println("Email Successfully Sent!");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+           
+	}
+
+}
