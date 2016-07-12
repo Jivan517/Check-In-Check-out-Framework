ed0bfd745c1375be56ec900f87e67ec217f0c18c
Umesh Paudyal <umesh.paudel.nepal@gmail.com>
2016-07-12 01:01:07 -0500

    made the return type as double for fine/fee getters and deleted the redundant ApplicationUser file

--- code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/presentation/Main.java
+++ code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/presentation/Main.java
@@ -1,5 +1,40 @@
 package cs525.rentalcarsystem.presentation;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private Stage primaryStage;
+	private Stage rootStage = new Stage();
+	private BorderPane rootLayout;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.rootStage = primaryStage;
+		this.primaryStage.setTitle("Rental System");
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("ApplicationUserForm.fxml"));
+			BorderPane page = (BorderPane) loader.load();
+			Scene scene = new Scene(page);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
