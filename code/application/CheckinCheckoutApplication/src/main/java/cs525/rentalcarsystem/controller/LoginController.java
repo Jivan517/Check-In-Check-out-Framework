4034aa0bb7aa839ee918aaa156d1d2462109d74d
Fisseha Abebe <abebefisseha5@gmail.com>
2016-07-11 23:42:05 -0500

    Some UI change on AddCustomerController

--- code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/controller/LoginController.java
+++ code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/controller/LoginController.java
@@ -42,7 +42,7 @@ public class LoginController extends Application {
 		});
 		
 	}
-	
+	@FXML
 	public void login(){
 		
 		
--- code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/presentation/Main.java
+++ code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/presentation/Main.java
@@ -1,5 +1,39 @@
 package cs525.rentalcarsystem.presentation;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	private Stage primaryStage;
+	private Stage rootStage = new Stage();
+	private BorderPane rootLayout;
+
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.rootStage = primaryStage;
+		this.primaryStage.setTitle("Car Rental Management System");
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("AddCustomerForm.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Scene scene = new Scene(page);
+			Stage ps = new Stage();
+			ps.setScene(scene);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
