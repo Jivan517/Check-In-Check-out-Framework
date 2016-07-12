05f6b38c6edd55902181f2e7d60c7f77b2d93aff
Jivan517 <np.jiv004@gmail.com>
2016-07-11 22:50:55 -0500

    sql scripts added

---
+++ docs/sql/INSERT_QUERY_CARRENTAL.sql
@@ -0,0 +1,90 @@
+create schema carrentalsystem;
+
+use carrentalsystem;
+
+/*DROP table APplicationUser
+
+*/
+
+CREATE TABLE ApplicationUser
+(
+sysuserId int NOT NULL AUTO_INCREMENT,
+firstName varchar(255) NOT NULL,
+lastName varchar(255),
+middleName varchar(255),
+phone varchar(255),
+email varchar(255),
+PRIMARY KEY (sysUserId)
+)
+
+CREATE TABLE Address
+(
+addressId int NOT NULL AUTO_INCREMENT,
+personId int (11) NOT NULL,
+`isCustomer` BIT NOT NULL,
+streetAddress varchar(255) NOT NULL,
+state varchar(255),
+city varchar(255),
+zipCode varchar(255),
+country varchar(255),
+PRIMARY KEY (addressid)
+
+        
+)ENGINE=INNODB;
+
+CREATE TABLE Customer
+(
+customerId int NOT NULL AUTO_INCREMENT,
+ 
+firstName varchar(255) NOT NULL,
+lastName varchar(255),
+middleName varchar(255),
+email varchar(255),
+phone varchar(255),
+PRIMARY KEY (customerid)
+ 
+        
+)ENGINE=INNODB;
+
+
+CREATE TABLE Car
+(
+productId int NOT NULL AUTO_INCREMENT,
+ 
+model varchar(255) NOT NULL,
+color varchar(255),
+releaseYear int,
+make varchar(255),
+rentalFeePerDay double,
+overduefinePerDay double,
+quantity int,
+carStatus varchar(255),
+plate varchar(255),
+mileage int,
+PRIMARY KEY (productId)
+ 
+        
+)ENGINE=INNODB;
+
+ALTER TABLE `carrentalsystem`.`car` 
+ADD COLUMN `name` VARCHAR(45) NULL AFTER `mileage`,
+ADD COLUMN `description` VARCHAR(45) NULL AFTER `name`;
+
+CREATE TABLE CheckoutRecordEntry
+(
+checkoutRecordEntryId int NOT NULL AUTO_INCREMENT,
+customerId int not null,
+personId int,
+carId int not null,
+quantity int,
+checkoutDate datetime,
+duedate datetime,
+returnedDate datetime,
+isReturned bit,
+rentalFee double,
+rentalFine double,
+
+PRIMARY KEY (checkoutRecordEntryId)
+ 
+        
+)ENGINE=INNODB;
