a49123ac54c5d88e2da0748f73152c85b48d3661
Fisseha Abebe <abebefisseha5@gmail.com>
2016-07-13 17:40:27 -0500

    dbconfig

--- code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/controller/AddCustomerController.java
+++ code/application/CheckinCheckoutApplication/src/main/java/cs525/rentalcarsystem/controller/AddCustomerController.java
@@ -30,6 +30,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -98,6 +99,7 @@ public class AddCustomerController extends Application implements Initializable 
 		try {
 			FXMLLoader loader = new FXMLLoader(Main.class.getResource("AddCustomerForm.fxml"));
 			AnchorPane page = (AnchorPane) loader.load();
+			primaryStage.getIcons().add(new Image("file:resources/images/icon.png"));
 			Scene scene = new Scene(page);
 			Stage ps = new Stage();
 			ps.setScene(scene);
